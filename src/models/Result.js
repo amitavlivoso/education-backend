c938b1530e4b4e098dc809d3289ceec42e645224
jyotilivoso <jagatjyoti@livosotech.com>
2025-08-12 10:04:33 +0530

    .

--- src/models/Result.js
+++ src/models/Result.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db.config');
 
-// models/Result.js
+//models/Result.js
 
   const Result = sequelize.define('Result', {
     examId: {
